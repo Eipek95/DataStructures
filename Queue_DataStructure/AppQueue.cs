8c6369cf7173d90b53271e7d3fa02ae8e48aa423
Emre <Emre@DESKTOP-THOD234>
2021-07-09 17:49:43 +0300

    Queue

---
+++ Queue_DataStructure/AppQueue.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queue_DataStructure
+{
+    //kullanıcının isteeğine göre sesli harfleri kuyruğa ekleme
+    public class AppQueue
+    {
+        public AppQueue()
+        {
+            methodVowel();
+        }
+
+        public void methodVowel()
+        {
+            var sesliHarfler = new List<char>()
+            {
+                'a','e','ı','i','u','ü','o','ö'
+            };
+            ConsoleKeyInfo secim;
+            var kuyruk = new Queue<char>();
+           
+            foreach (var item in sesliHarfler)
+            {
+                Console.WriteLine($"{item,-5} kuyruğa eklensin mi? [e/h] ");
+                secim=Console.ReadKey();
+                Console.WriteLine();
+                if (secim.Key==ConsoleKey.E)
+                {
+                    kuyruk.Enqueue(item);
+                    Console.WriteLine($"\n{item,-5} kuyruğa kuyruğa eklendi");
+                    Console.WriteLine($"Kuyruktaki eleman sayısı: { kuyruk.Count}");
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Kuyruktan Elemanlarin kaldirilmasi için Esc tuşuna basınız");
+            secim = Console.ReadKey();
+
+            if (secim.Key==ConsoleKey.Escape)
+            {
+                Console.WriteLine();
+
+                while (kuyruk.Count > 0)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"{kuyruk.Peek()} kuyruktan çıkartılıyor");
+                    Console.WriteLine($"{kuyruk.Dequeue(),5} kuyruktan çıkarıldı");
+                    Console.WriteLine($"Kuyruktaki eleman sayısı: { kuyruk.Count}");
+                }
+                Console.WriteLine("\n Kuyruktan Çıkarılma İşlemi Tamamlandı");
+
+            }
+            Console.WriteLine("Program Bitti");
+            
+        }
+    }
+}
---
+++ Queue_DataStructure/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queue_DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppQueue appQueue = new AppQueue();
+         
+        }
+
+        private static void queueStructure()
+        {
+            //oluşturma
+            var karakterKuyrugu = new Queue<char>();
+            //ekleme
+            karakterKuyrugu.Enqueue('A');
+            karakterKuyrugu.Enqueue('E');
+            Console.WriteLine($"Eleman Sayısı: {karakterKuyrugu.Count}");
+
+            //diziye ekleme
+            var dizi = karakterKuyrugu.ToArray();
+
+            Console.WriteLine($"Kuyruğun başındaki eleman: {karakterKuyrugu.Peek()}");
+            Console.WriteLine($"Kuyruktan çıkarılan eleman: {karakterKuyrugu.Dequeue()}");
+            Console.WriteLine($"Eleman Sayısı: {karakterKuyrugu.Count}");
+            Console.WriteLine($"Kuyruğun başındaki eleman: {karakterKuyrugu.Peek()}");
+        }
+    }
+}
